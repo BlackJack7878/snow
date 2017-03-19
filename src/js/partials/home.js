85dee2acfeb448f08dea37335b6f39423c90c3fb
BlackJack7878 <oleg55862@gmail.com>
2017-03-19 03:14:33 +0200

    blog & portfolio page, fixes for main
    - main page projects imgs position fix
    - header anchors links

--- src/js/partials/home.js
+++ src/js/partials/home.js
@@ -12,7 +12,22 @@ $(document).ready(function() {
 		arrows: false
 	});
 
-	$('.mainheader-arrow').click(function(){
+	// scrolling to anchor from another page
+	// *only* if we have anchor on the url
+	if(window.location.hash.length > 0) {
+		console.log('test');
+    	// set view at the top
+    	scroll(0,0);
+    	setTimeout( function() { scroll(0,0); }, 1);
+
+        // smooth scroll to the anchor id
+        $('html, body').animate({scrollTop:
+        	$(window.location.hash)
+        	.position()
+        	.top - 100}, 2000);
+    }
+
+	$('.mainheader-arrow, .anchor').click(function(){
 		var target = $(this).attr('to');
 		$('html, body').animate({scrollTop:$(target).position().top - 100}, 2000);
 	});
@@ -20,10 +35,12 @@ $(document).ready(function() {
 	var x = false;
 	$('.cat-btn').on('click', function(){
 		if (!x) {
+			$('.cat-menu-item').css('visibility', 'visible');
 			$('.cat-menu-item').css('opacity', '1');
 			x = true;
 		}
 		else {
+			$('.cat-menu-item').css('visibility', 'hidden');
 			$('.cat-menu-item').css('opacity', '0');
 			x = false;
 		}
